da1de6952930cfc3362d2f6fd76f4d47170e3b58
Sachin Dube <sachin.dube1@hmcts.net>
2019-11-19 13:38:14 +0000

    Fizzbuzz application Stage 2 requirements - refactored

--- src/main/java/com/app/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/app/fizzbuzz/FizzBuzz.java
@@ -10,14 +10,15 @@ public class FizzBuzz {
     private static final String fizzBuzz = "FizzBuzz";
     private static final String fizz = "Fizz";
     private static final String buzz = "Buzz";
-    private static final Function<Integer,Integer> divisibleByThree = x -> x % 3;
-    private static final Function<Integer,Integer> divisibleByFive = x -> x % 5;
+    private static final Function<Integer,Boolean> divisibleByThree = x -> x % 3 == 0;
+    private static final Function<Integer,Boolean> divisibleByFive = x -> x % 5 == 0;
 
     public static String fizzBuzz(Integer number) {
         return Optional.of(number).map(n -> {
-            if ((divisibleByThree.apply(n) == 0|| containsDigit(n,3)) && (divisibleByFive.apply(n) == 0 || containsDigit(n,5))) return fizzBuzz;
-            else if (divisibleByThree.apply(n) == 0 || containsDigit(n,3)) return fizz;
-            else if ((divisibleByFive.apply(n) == 0) || containsDigit(n,5)) return buzz;
+            if ((divisibleByThree.apply(n) || containsDigit(n,3)) &&
+                    (divisibleByFive.apply(n) || containsDigit(n,5))) return fizzBuzz;
+            else if (divisibleByThree.apply(n) || containsDigit(n,3)) return fizz;
+            else if (divisibleByFive.apply(n) || containsDigit(n,5)) return buzz;
             else return n.toString();
         }).get();
     }
