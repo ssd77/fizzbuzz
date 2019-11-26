0049e07ec964311cdea3e31bd915ded28dfe7004
Sachin Dube <sachin.dube1@hmcts.net>
2019-11-26 12:06:31 +0000

    New approach stage 2 requirements

--- src/main/java/com/app/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/app/fizzbuzz/FizzBuzz.java
@@ -2,16 +2,19 @@ package com.app.fizzbuzz;
 
 import static java.util.stream.Collectors.joining;
 
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class FizzBuzz implements Application {
     private static final String fizz  = "Fizz";
     private static final String buzz = "Buzz";
-    private static java.util.function.Predicate<Integer> divBy(int n) { return i -> (i % n) == 0;}
+    private static Predicate<Integer> contains3 = str -> String.valueOf(str).contains("3");
+    private static Predicate<Integer> contains5 = str -> String.valueOf(str).contains("5");
+    private static Predicate<Integer> divBy(int n) { return i -> (i % n) == 0;}
 
     String fizzBuzz(Integer number){
-        String showFizz = divBy(3).test(number) ? fizz : "";
-        String showBuzz = divBy(5).test(number) ? buzz : "";
+        String showFizz = divBy(3).or(contains3).test(number) ? fizz : "";
+        String showBuzz = divBy(5).or(contains5).test(number) ? buzz : "";
         return (showFizz.isEmpty() && showBuzz.isEmpty()) ? number.toString() : showFizz + showBuzz;
     }
 
